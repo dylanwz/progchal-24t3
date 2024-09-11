12634b53a542a66186e2c4521337079e2dadff0f
dylan <birds@DylanW-PC>
2024-09-11 22:23:22 +1000

    initial commit

---
+++ lecture1-admin.cpp
@@ -0,0 +1,30 @@
+/*
+Topics:
+  1.  Problem Solving Paradigms
+  2.  Paradigms
+  3.  Data Structures
+  4.  DP
+  5.  Graph Algorithms
+  7.  Shortest Paths (reduce some problem to famous graph problem)
+  8.  Flow Networks
+  9.  Mathematics
+  10. Computational Geometry
+
+Weeklys:
+  - 90 minute F2F workshop (usually two example problems based on recent lectures)
+    - problem diary marked at workshop
+  - weekly problem set
+  - contests
+
+Assessment Breakdown:
+  - 40%:  Weekly problem sets (on vjudge) (documented via problem diary)
+    - PS = 1ps, CR = 2ps, DN = 3ps, HD = 4ps
+  - 8%:   Problem diary (on formatif)
+  - 18%:  Contests (week 1, week 5 and week 9; 3 problems each)
+  - 34%:  Final (7 problems over 5 hours)
+  NB:     Course has NO DEADLINES.
+
+Problem Diary:
+  - documents weekly problem sets
+  - written on formatif, submitted/marked in workshop
+*/
