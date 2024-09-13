2e0cdfbb06f8a6feb9f35591523e5514873222f8
dylan <birds@DylanW-PC>
2024-09-13 23:53:14 +1000

    13/09

--- pset1/A.cpp
+++ pset1/A.cpp
@@ -2,11 +2,28 @@
 #include <string>
 #include <vector>
 
-void baby_bites(int n, vector<string> s) {
-    int count = 1;
+using namespace std;
+
+void baby_bites() {
+    int r = 1;
     
+    int n; cin >> n;
+    for (size_t i = 0; i < n; i++) {
+        string bf; cin >> bf;
+        
+        // something is fishy if the number spoken doesn't match the right number
+        if (bf != "mumble" && stoi(bf) != r) {
+            cout << "something is fishy" << "\n";
+            return;
+        }
+
+        r++;
+    }
+
+    cout << "makes sense";
+
 }
 
 int main() {
-
+    baby_bites();
 }
